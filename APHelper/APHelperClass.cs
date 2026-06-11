bdff3b555e8718695f3d0b98795ef19aefeacd61
lexipherous <jasonnlelong@gmail.com>
2026-06-11 15:41:03 +0100

    Updated Collection Hooks

--- APHelper/APHelperClass.cs
+++ APHelper/APHelperClass.cs
@@ -16,17 +16,36 @@ namespace APHelper {
     public partial class APHelperClass : MelonMod {
         public override void OnInitializeMelon() {
             MethodInfo originalPickableItem = AccessTools.Method(typeof(Gameplay_PickableItem), "Pickup");
-            MethodInfo patchPickableItem = AccessTools.Method(typeof(Patches.Patches), "CustomPickableItem");
+            MethodInfo patchPickableItem = AccessTools.Method(typeof(Patches.Patches), "APItemPickup");
             HarmonyInstance.Patch(originalPickableItem, new HarmonyMethod(patchPickableItem));
-            
+                
             MethodInfo originalInteractableAbsorbable = AccessTools.Method(typeof(Gameplay_InteractableAbsorbable), "OnInteract");
-            MethodInfo patchInteractableAbsorbable = AccessTools.Method(typeof(Patches.Patches), "CustomInteractableAbsorbable");
+            MethodInfo patchInteractableAbsorbable = AccessTools.Method(typeof(Patches.Patches), "APInteractAbsorb");
             HarmonyInstance.Patch(originalInteractableAbsorbable, new HarmonyMethod(patchInteractableAbsorbable));
             
             MethodInfo originalObjectAbsorbHandler = AccessTools.Method(typeof(Gameplay_ObjectAbsorbHandler), "OnSelfPullStart");
-            MethodInfo patchObjectAbsorbHandler = AccessTools.Method(typeof(Patches.Patches), "CustomObjectAbsorbHandler");
+            MethodInfo patchObjectAbsorbHandler = AccessTools.Method(typeof(Patches.Patches), "APObjectAbsorb");
             HarmonyInstance.Patch(originalObjectAbsorbHandler, new HarmonyMethod(patchObjectAbsorbHandler));
-            Melon<APHelperClass>.Logger.Msg("[APHelper] Initialized! :D");
+            
+            MethodInfo originalAbsorbableCluster = AccessTools.Method(typeof(AbsorbableCluster_Event), "ReleaseContent");
+            MethodInfo patchAbsorbableCluster = AccessTools.Method(typeof(Patches.Patches), "APMarahBloodroot");
+            HarmonyInstance.Patch(originalAbsorbableCluster, new HarmonyMethod(patchAbsorbableCluster));
+            
+            // NPC Hook
+            //MethodInfo originalProfile_NPC = AccessTools.Method(typeof(Profile_NPC), "getInitialConversationID");
+            //MethodInfo patchProfile_NPC = AccessTools.Method(typeof(LocationSend), "AP_NPC");
+            //HarmonyInstance.Patch(originalProfile_NPC, new HarmonyMethod(patchProfile_NPC));
+            
+            // Checkpoint Hook
+            MethodInfo originalCheckpointHandler = AccessTools.Method(typeof(Gameplay_CheckpointHandler), "OnInteract");
+            MethodInfo patchCheckpointHandler = AccessTools.Method(typeof(Patches.Patches), "APCheckpoint");
+            HarmonyInstance.Patch(originalCheckpointHandler, new HarmonyMethod(patchCheckpointHandler));
+            
+            // Beacon Hook
+            MethodInfo originalBeaconHandler = AccessTools.Method(typeof(BeaconHandler), "OnInteract");
+            MethodInfo patchBeaconHandler = AccessTools.Method(typeof(Patches.Patches), "APBeacon");
+            HarmonyInstance.Patch(originalBeaconHandler, new HarmonyMethod(patchBeaconHandler));
+            Melon<APHelperClass>.Logger.Msg("Wohoo! Initialized! :D");
         }
     }
 }
--- APHelper/Patches/Patches.cs
+++ APHelper/Patches/Patches.cs
@@ -10,13 +10,20 @@ public class Patches
     /// Hook for items.
     /// This seems to include: white items, purple items, atrium, fragments. Force Cap items untested.
     /// </summary>
-    public static bool CustomPickableItem(Gameplay_PickableItem __instance)
-    {
-        Melon<APHelperClass>.Logger.Msg("syncerID: " + __instance.syncer.syncerID +
-                                        " - areaName: " + __instance.gameObject.scene.name +
-                                        " - itemName: " + __instance.targetItem.name +
-                                        " - itemQuantity: " + __instance.amount +
-                                        " - itemXp: " + __instance.expGainTerm.Length);
+    public static bool APItemPickup(Gameplay_PickableItem __instance, bool instant) {
+        // Replace current item with obsolete hidden item
+        var newItem = GetItemById("6c96f712-0211-4c1f-a794-cdb781a574dc");
+        __instance.targetItem = newItem;
+        
+        foreach (var i in __instance.absorbableAdditionals) {
+            Melon<APHelperClass>.Logger.Msg(i.name);
+        }
+        
+        if (__instance.pickupType == Gameplay_PickableItem.PickupType.Exp) {__instance.amount = 0;}
+        
+        Melon<APHelperClass>.Logger.Msg($"ItemPickup Additional: syncerID: {__instance} - Count:{__instance.absorbableAdditionals.Count}");
+
+        SendHandler("ItemPickup", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
         return true;
     }
 
@@ -24,9 +31,12 @@ public class Patches
     /// Hook for interactibles.
     /// This seems to include: Overgrown Barrier.
     /// </summary>
-    public static bool CustomInteractableAbsorbable(Gameplay_InteractableAbsorbable __instance)
-    {
-        Melon<APHelperClass>.Logger.Msg($"IA: syncerID: {__instance.syncer.syncerID} - {__instance.name}");
+    public static bool APInteractAbsorb(Gameplay_InteractableAbsorbable __instance) {
+        //var newType = RewardType.AP_Pickup;
+        __instance.rewardType = (Gameplay_InteractableAbsorbable.RewardType)3;
+        
+        var newItem = __instance.PickUp();
+        SendHandler("InteractAbsorb", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
         return true;
     }
 
@@ -34,9 +44,73 @@ public class Patches
     /// Hook for interactibles.
     /// This seems to include: Lahav Knight force drops
     /// </summary>
-    public static bool CustomObjectAbsorbHandler(Gameplay_ObjectAbsorbHandler __instance)
-    {
-        Melon<APHelperClass>.Logger.Msg($"OAH: syncerID: {__instance.syncer.syncerID} - Name: {__instance.name} - isFlesh: {__instance.absorbFlesh} - graspText: {__instance.graspText}");
+    public static bool APObjectAbsorb(Gameplay_ObjectAbsorbHandler __instance) {
+        SendHandler("ObjectAbsorb", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
         return true;
+    }
+
+    /// <summary>
+    /// Hook for Bloodroot and Marah Strand.
+    /// This seems to include: Lahav Knight force drops
+    /// </summary>
+    public static bool APMarahBloodroot(AbsorbableCluster_Event __instance) {
+        SendHandler("MarahBloodroot", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
+        return true;
+    }
+
+    /// <summary>
+    /// </summary>
+    //public static bool AP_NPC(Profile_NPC __instance) {
+    //    SendHandler("AP_NPC", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
+    //    return true;
+    //}//
+
+    /// <summary>
+    /// </summary>
+    public static bool APCheckpoint(Gameplay_CheckpointHandler __instance) {
+        SendHandler("Checkpoint", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
+        return true;
+    }
+
+    /// <summary>
+    /// </summary>
+    public static bool APBeacon(BeaconHandler __instance) {
+        SendHandler("Beacon", __instance.gameObject.GetComponent<Syncer>().syncerID, __instance.gameObject.scene.name, __instance.gameObject.transform.position);
+        return true;
+    }
+
+    public static void SendHandler(string source, long syncerID, string areaName, Vector3 pos) {
+        string sendString = $"{source}|[\"{NameToIDName(areaName)}:{syncerID}\"] = new LocationEnum(0, \"{GetAreaFromSceneName(areaName)}\"), // {pos}";
+        Melon<APHelperClass>.Logger.Msg(sendString);
+        //SendLocation(syncerID);
+    }
+    
+    private static string NameToIDName(string fullSceneName) {
+        // Input: "00_Tutorial_01_BAKED_BASE_HARD_HIGH_LAYER 0"
+        // Output: "00_Tutorial_01"
+        
+        if (fullSceneName.Contains("_BAKED"))
+        {
+            fullSceneName = fullSceneName.Substring(0, fullSceneName.IndexOf("_BAKED"));
+        }
+    
+        return fullSceneName;
+    }
+    
+    private static string GetAreaFromSceneName(string cleanSceneName) {
+        var parts = cleanSceneName.Split('_');
+        return parts[1];
+    }
+    
+    public static Data_Item GetItemById(string itemId) {
+        Hashtable_Items hashtable = Hashtable_Items.getHashtable;
+        if (hashtable == null) {
+            Melon<APHelperClass>.Logger.Msg("Hashtable_Items.getHashtable is null.");
+        }
+        Data_Item item = hashtable.GetItemByID(itemId);
+        if (item == null) {
+            Melon<APHelperClass>.Logger.Msg($"Could not find item with id '{itemId}'.");
+        }
+        return item;
     }
 }
